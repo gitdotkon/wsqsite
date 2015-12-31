2a08ba67202c3d5a506fed8236432a5f9b5d0d4d
slim <serene.lim@questql.com>
2015-12-31 10:43:43 +0800

    cc mail final + iPad nav

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'success',
-		'message'=>'<br><p style="font-size:1.8em; padding:0.5em 0; color:#07baba">Thank You</p><p style="color:#fff;  font-size:14px; line-height:19px; margin:0 0 10px;">Thank you for your interest in Wanda Studios Qingdao. A representative will be in touch with you shortly.</br></br><p><br>'
+		'message'=>'<br><p style="font-size:1.8em; padding:0.5em 0; color:#07baba">Thank You</p><p style="color:#fff;  font-size:14px; line-height:19px; margin:0 0 10px;">Thank you for your interest in Wanda Studios Qingdao. A representative will be in touch with you as soon as possible.</br></br><p><br>'
 	);
 
     $name = @trim(stripslashes($_POST['name'])); 
@@ -12,10 +12,10 @@
     $message = @trim(stripslashes($_POST['message'])); 
    
     $email_from = $email;
-    $email_to = 'info@questql.com';//replace with your email
+    $email_to = 'info@wandastudios.com';//replace with your email
 	
-	$body =  '<p>Hi Admin -</p>';
-	$body .= '<p>You have an enquiry. Please find customer\'s enquiry details below:--</p>';	
+	$body =  '<p>Hi Admin</p>';
+	$body .= '<p>You have an enquiry from wandastudios.com. Please find customer\'s enquiry details below:--</p>';	
     $body .= '<b>Name: </b>' . $name . "<p></p>"  
 			. '<b>Email: </b>' . $email . "<p></p>" 
 			. '<b>Subject: </b>' . $subject . "<p></p>" 
@@ -23,7 +23,7 @@
 			. '<b>Company: </b>' . $company."<p></p>";
 			
 	$body .= "<p>Regards- </br>"
-			."WSQ Team</p>";
+			."Wandastudios.com</p>";
 			
 
 	// To send HTML mail, the Content-type header must be set
@@ -31,10 +31,10 @@
 	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 	
 	// Additional headers
-	$headers .= 'To: WSQ <info@questql.com>' . "\r\n";
+	$headers .= 'To: WSQ <info@wandastudios.com>' . "\r\n";
 	$headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
-	//$headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
-	$headers .= 'BCc:  '.$name.' <'.$email.'>' . "\r\n";
+	//$headers .= 'BCc: birthdayarchive@example.com' . "\r\n";
+	$headers .= 'Cc:  '.$name.' <'.$email.'>' . "\r\n";
 	
    
 	// Mail it
