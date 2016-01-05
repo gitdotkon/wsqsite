96678730fbea422dcb9fc076462f9a71a4cfa2ee
slim <serene.lim@questql.com>
2016-01-05 21:16:11 +0800

    test

--- js/wxapi.js
+++ js/wxapi.js
@@ -10,12 +10,48 @@ function is_weixn(){
 if(is_weixn()){
 
         var imgUrl = "http://staging.wandastudios.com/images/fx.jpg";
-        var lineLink = "http://www.wandastudios.com";
-        var descContent = 'Get to know more about Wanda Studios Qingdao.';
-        var shareTitle = 'Wandastudios.com';
+        var lineLink = "http://staging.wandastudios.com/";
+        var descContent = '';
+        var shareTitle = 'Wanda Studios Qingdao';
         var appid = '';
         
-        
+        function shareFriend() {
+			if(Xmas.jianfei()){descContent=''}
+			//else{descContent=''}
+            WeixinJSBridge.invoke('sendAppMessage',{
+                "appid": appid,
+                "img_url": imgUrl,
+                "img_width": "120",
+                "img_height": "120",
+                "link": lineLink,
+                "desc": descContent,
+                "title": shareTitle
+            }, function(res) {
+                Xmas.closefx();
+            })
+        }
+        function shareTimeline() {
+			if(Xmas.jianfei()){descContent=''}
+            WeixinJSBridge.invoke('shareTimeline',{
+                "img_url": imgUrl,
+                "img_width": "120",
+                "img_height": "120",
+                "link": lineLink,
+                "desc": descContent,
+                "title": shareTitle
+            }, function(res) {
+                   Xmas.closefx();
+            });
+        }
+        function shareWeibo() {
+			if(Xmas.jianfei()){descContent=''}
+            WeixinJSBridge.invoke('shareWeibo',{
+                "content": descContent,
+                "url": lineLink,
+            }, function(res) {
+                Xmas.closefx();
+            });
+        }
         // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
         document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
             // 发送给好友
